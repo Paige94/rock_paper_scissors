8369a028672e1596bf6badb6ad984c8f8b05f681
“Paige94” <25869300+Paige94@users.noreply.github.com>
2024-01-22 12:41:24 +0000

    Add capability to play one round against computer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Get random computer chocie of rock, paper or scissors
+// Get random computer choice of rock, paper or scissors
 function getComputerChoice () {
     let randomNumber = Math.floor((Math.random() * 3) + 1);
     if (randomNumber === 1) {
@@ -6,7 +6,7 @@ function getComputerChoice () {
     } else if (randomNumber === 2) {
         return "paper";
     } else if (randomNumber === 3) {
-        return "scisscors";
+        return "scissors";
     }
 }
 
@@ -18,8 +18,23 @@ function getPlayerChoice () {
 }
 
 // Play one round of the game: computer vs player
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You lose! Rock beats scissors!";
+    } else if (computerSelection === "rock" && playerSelection === "paper") {
+        return "You win! Paper beats rock!";
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "You lose! Paper beats rock!";
+    } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        return "You win! Scissors beat paper!";
+    } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return "You win! Rock beats scissors!";
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return "You lose! Scissors beat paper!";
+    } else if ((computerSelection === "rock" && playerSelection === "rock") || (computerSelection === "paper" && playerSelection === "paper") || (computerSelection === "scissors" && playerSelection === "scissors")) {
+        return "It's a tie! You chose the same thing!";
+    }
+    }
 
 
-
-
-console.log(getPlayerChoice());
+console.log(playRound(getComputerChoice(), getPlayerChoice()));
