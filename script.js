b995c229eda0cd6b2a6b9d847fd54aa7f48e6d06
“Paige94” <25869300+Paige94@users.noreply.github.com>
2024-01-22 12:16:39 +0000

    Add logic to get computer choice in game

---
+++ script.js
@@ -0,0 +1,14 @@
+// Get random computer chocie of rock, paper or scissors
+function getComputerChoice () {
+    let randomNumber = Math.floor((Math.random() * 3) + 1);
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else if (randomNumber === 3) {
+        return "scisscors";
+    }
+
+}
+
+console.log(getComputerChoice());
