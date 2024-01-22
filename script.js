84e10becdd23761b7f7e42aa9962f129b7c9e888
“Paige94” <25869300+Paige94@users.noreply.github.com>
2024-01-22 12:22:42 +0000

    Add way to get player's choice for game

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ function getComputerChoice () {
     } else if (randomNumber === 3) {
         return "scisscors";
     }
-
 }
 
-console.log(getComputerChoice());
+// Get player's choice of rock, paper or scissors
+function getPlayerChoice () {
+    let playerChoice = prompt("Let's play! Choose rock, paper or scissors");
+    playerChoice = playerChoice.toLowerCase();
+    return playerChoice;
+}
+
+// Play one round of the game: computer vs player
+
+
+
+
+console.log(getPlayerChoice());
